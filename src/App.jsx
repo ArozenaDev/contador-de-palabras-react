868ed52dde632eced46f23a131f42c1cc6f822b3
Jose Arozena <josarozena@gmail.com>
2023-11-12 23:16:03 +0100

    Cambiados nombres de variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState('');
   const [sentence, setSentence] = useState('');
-  const [tempSentence, setTempSentence] = useState('');
 
   return (
     <>
     <h1>Contador de palabras</h1>
-        <textarea value={tempSentence} 
-        onChange={(e) => setTempSentence(e.target.value)} 
+        <textarea value={sentence} 
+        onChange={(e) => setSentence(e.target.value)} 
         name="text" rows="20" cols="60" 
         placeholder="Introduce un texto..." />
         <br/>
-        <input type="button" id="go" value="Contar" onClick={() => setSentence(tempSentence.split(' ').length)} />
-        <div id="result">Total de palabras: {sentence}</div>
+        <input type="button" id="go" value="Contar" onClick={() => setCount(sentence.split(' ').length)} />
+        <div id="result">Total de palabras: {count}</div>
     </>
   )
 }
