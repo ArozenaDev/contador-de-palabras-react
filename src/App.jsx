7aea59be9c24ef73bfc1bdca1dbf3ca994307690
Jose Arozena <josarozena@gmail.com>
2023-11-12 12:08:23 +0100

    añadido contador de palabras

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 
 function App() {
-  const [, ] = useState(0)
+  const [sentence, setSentence] = useState('');
+  const [tempSentence, setTempSentence] = useState('');
 
   return (
     <>
     <h1>Contador de palabras</h1>
-        <textarea id="input" name="text" rows="20" cols="60" placeholder="Introduce un texto..." />
+        <textarea value={tempSentence} 
+        onChange={(e) => setTempSentence(e.target.value)} 
+        name="text" rows="20" cols="60" 
+        placeholder="Introduce un texto..." />
         <br/>
-        <input type="button" id="go" value="Contar" />
-        <div id="result" />
+        <input type="button" id="go" value="Contar" onClick={() => setSentence(tempSentence.split('').length)} />
+        <div id="result">{sentence}</div>
     </>
   )
 }
